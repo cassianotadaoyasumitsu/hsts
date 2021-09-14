53a1bff9b544612d0664e7f48cfcfdfa98c71f6c
cassiano yasumitsu <cassiano@usuarioincomum.com>
2021-09-14 12:22:55 +0900

    first hsts go

---
+++ go.mod
@@ -0,0 +1,5 @@
+module example.com/hsts
+
+go 1.17
+
+require github.com/a-h/hsts v0.0.0-20170713145656-509101faf0de // indirect
---
+++ main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"net/http"
+
+	"github.com/a-h/hsts"
+)
+
+var serveHTTPSFlag = flag.Bool("serveHTTPS", false, "Whether the system should serve SSL using the example certificate.")
+
+func main() {
+	if *serveHTTPSFlag {
+		serveHTTPS()
+	} else {
+		serveHTTP()
+	}
+}
+
+func serveHTTPS() {
+	log.Print("Serving HTTPS to demonstrate header.")
+
+	http.Handle("/hellohandler", hsts.NewHandler(&structHandler{}))
+	http.Handle("/hellofunction", hsts.NewHandler(http.HandlerFunc(serveFunction)))
+	log.Fatal(http.ListenAndServeTLS(":443", "server.pem", "server.key", nil))
+}
+
+func serveHTTP() {
+	redirectToSSL := true
+
+	if redirectToSSL {
+		log.Print("Serving with HTTP to HTTPS redirection.")
+
+		http.Handle("/hellohandler", hsts.NewHandler(&structHandler{}))
+		http.Handle("/hellofunction", hsts.NewHandler(http.HandlerFunc(serveFunction)))
+		log.Fatal(http.ListenAndServe(":8080", nil))
+	} else {
+		log.Print("Serving without HTTP to HTTPS redirection.")
+
+		http.Handle("/hellohandler", &structHandler{})
+		http.HandleFunc("/hellofunction", serveFunction)
+		log.Fatal(http.ListenAndServe(":8080", nil))
+	}
+}
+
+type structHandler struct {
+}
+
+func (h *structHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello from the Handler interface."))
+}
+
+func serveFunction(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello from a function"))
+}
